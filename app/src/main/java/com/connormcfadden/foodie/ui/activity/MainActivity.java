ab2bc5705bef9ee74099e169ee51e4f6e8a88b3f
connor <connor@memrise.com>
2014-09-17 20:20:02 +0100

    Nav drawer implemented

--- app/src/main/java/com/connormcfadden/foodie/ui/activity/MainActivity.java
+++ app/src/main/java/com/connormcfadden/foodie/ui/activity/MainActivity.java
@@ -1,7 +1,91 @@
 package com.connormcfadden.foodie.ui.activity;
 
 import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.connormcfadden.foodie.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends Activity {
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @InjectView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        setupNavDrawer();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        /* todo use later for adding recipe
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_sign_out:
+                handleUserSignOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        */
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupNavDrawer() {
+        //mNavDrawerLearn.setSelected(true);
+        //mNavDrawerLearn.setTypeface(Typeface.DEFAULT_BOLD);
+        //setListenerNavDrawerItems();
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_launcher,
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            private boolean isOpen;
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                isOpen = false;
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                isOpen = true;
+            }
+        };
+
+       // mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
 
 }
